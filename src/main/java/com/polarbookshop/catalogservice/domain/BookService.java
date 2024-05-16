6bdcc05c4bbd40122e07a9e862f02ee55738b6cb
Elvin <elvin@gccompany.co.kr>
2024-05-16 18:35:02 +0900

    chapter5-2 컨테이너 생성 및 PostgreSQL JDBC 설정

--- src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
@@ -17,7 +17,7 @@ public class BookService {
 
     public Book viewBookDetails(String isbn) {
         return bookRepository.findByIsbn(isbn)
-                .orElseThrow(() -> new BookNotFoundException(isbn));
+            .orElseThrow(() -> new BookNotFoundException(isbn));
     }
 
     public Book addBookToCatalog(Book book) {
@@ -32,16 +32,20 @@ public class BookService {
     }
 
     public Book editBookDetails(String isbn, Book book) {
-		return bookRepository.findByIsbn(isbn)
-				.map(existingBook -> {
-					var bookToUpdate = new Book(
-							existingBook.isbn(),
-							book.title(),
-							book.author(),
-							book.price());
-					return bookRepository.save(bookToUpdate);
-				})
-				.orElseGet(() -> addBookToCatalog(book));
+        return bookRepository.findByIsbn(isbn)
+            .map(existingBook -> {
+                var bookToUpdate = new Book(
+                    existingBook.id(),
+                    existingBook.isbn(),
+                    book.title(),
+                    book.author(),
+                    book.price(),
+                    existingBook.createdDate(),
+                    existingBook.lastModifiedDate(),
+                    existingBook.version());
+                return bookRepository.save(bookToUpdate);
+            })
+            .orElseGet(() -> addBookToCatalog(book));
     }
 
 }
